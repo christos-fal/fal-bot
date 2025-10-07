bc9bf356b820d7dad881b329f7939da4aee7a859
christos-fal <christos@fal.ai>
2025-10-07 16:24:29 -0700

    Add moderation checks for content safety in Hunyuan and Ovi commands;

--- fal_bot/config.py
+++ fal_bot/config.py
@@ -3,5 +3,7 @@ import os
 DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
 FAL_SECRET = os.environ["FAL_SECRET"]
 RAW_GUILD_ID = os.environ["GUILD_ID"]
+GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
+
 
 FALAI_LOGO_URL = "https://avatars.githubusercontent.com/u/74778219?s=200&v=4"
