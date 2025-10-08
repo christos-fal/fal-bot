80192e1fd51eecdb05c6080bedd4e27114a241e1
christos-fal <christos@fal.ai>
2025-10-08 15:44:36 -0700

    fix: config

--- fal_bot/config.py
+++ fal_bot/config.py
@@ -4,6 +4,6 @@ DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
 FAL_SECRET = os.environ["FAL_SECRET"]
 RAW_GUILD_ID = os.environ["GUILD_ID"]
 GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
-
+os.environ["FAL_KEY"] = FAL_SECRET
 
 FALAI_LOGO_URL = "https://avatars.githubusercontent.com/u/74778219?s=200&v=4"
